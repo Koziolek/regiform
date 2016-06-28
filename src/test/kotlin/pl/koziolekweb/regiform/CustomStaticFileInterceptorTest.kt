ec94462d2ef88be0783d8c6406f774c7483c0a61
Koziolek <bjkuczynski@gmail.com>
2016-06-28 20:55:40 +0200

    Fix tests

--- src/test/kotlin/pl/koziolekweb/regiform/CustomStaticFileInterceptorTest.kt
+++ src/test/kotlin/pl/koziolekweb/regiform/CustomStaticFileInterceptorTest.kt
@@ -2,9 +2,10 @@ package pl.koziolekweb.regiform
 
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
+import io.netty.handler.codec.http.DefaultHttpRequest
+import io.netty.handler.codec.http.HttpHeaders
 import io.netty.handler.codec.http.HttpMethod
-import io.netty.handler.codec.http.HttpRequest
-import org.fest.assertions.Assertions
+import org.fest.assertions.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.wasabi.http.Request
 import org.wasabi.http.Response
@@ -18,19 +19,69 @@ class CustomStaticFileInterceptorTest() : Spek({
 
         given("Custom static interceptor that has default configuration") {
 
-            val res = this.javaClass.getResource("/interceptor")
+            val res = "".javaClass.getResource("/interceptors")
             val sut = CustomStaticFileInterceptor(res.path)
 
-            it("GET -> return forward") {
-                val httpRequest: HttpRequest = mock()
+            it("GET / -> return true") {
+                val httpRequest: DefaultHttpRequest = mock()
                 whenever(httpRequest.method).thenReturn(HttpMethod.GET)
                 whenever(httpRequest.uri).thenReturn("/")
+                whenever(httpRequest.headers()).thenReturn(HttpHeaders.EMPTY_HEADERS)
 
-                val response: Response = mock()
+                val response: Response = Response()
                 val request = Request(httpRequest)
                 val intercept = sut.intercept(request, response)
 
-                Assertions.assertThat(intercept).isTrue();
+                assertThat(intercept).isTrue();
+            }
+
+            it("GET index html -> return false") {
+                val httpRequest: DefaultHttpRequest = mock()
+                whenever(httpRequest.method).thenReturn(HttpMethod.GET)
+                whenever(httpRequest.uri).thenReturn("/index.html")
+                whenever(httpRequest.headers()).thenReturn(HttpHeaders.EMPTY_HEADERS)
+
+                val response: Response = Response()
+                val request = Request(httpRequest)
+                val intercept = sut.intercept(request, response)
+
+                assertThat(intercept).isFalse();
+            }
+
+        }
+
+        given("Custom static interceptor that has extra configuration") {
+
+            val res = "".javaClass.getResource("/interceptors")
+            val sut = CustomStaticFileInterceptor(res.path, true)
+
+            it("GET / -> return false") {
+                val httpRequest: DefaultHttpRequest = mock()
+                whenever(httpRequest.method).thenReturn(HttpMethod.GET)
+                whenever(httpRequest.uri).thenReturn("/")
+                whenever(httpRequest.headers()).thenReturn(HttpHeaders.EMPTY_HEADERS)
+
+                val response: Response = Response()
+                val request = Request(httpRequest)
+                val intercept = sut.intercept(request, response)
+
+                assertThat(intercept).isFalse();
+                assertThat(response.contentType).isEqualTo("text/html");
+
+            }
+
+            it("GET index html -> return false") {
+                val httpRequest: DefaultHttpRequest = mock()
+                whenever(httpRequest.method).thenReturn(HttpMethod.GET)
+                whenever(httpRequest.uri).thenReturn("/index.html")
+                whenever(httpRequest.headers()).thenReturn(HttpHeaders.EMPTY_HEADERS)
+
+                val response: Response = Response()
+                val request = Request(httpRequest)
+                val intercept = sut.intercept(request, response)
+
+                assertThat(intercept).isFalse();
+                assertThat(response.contentType).isEqualTo("text/html");
             }
 
         }
